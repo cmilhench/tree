95c723aeb64487ff860ca36df9592fb61ebf813e
cmilhench <colinm@cm-group.co.uk>
2013-10-01 16:16:07 +0100

    Initial Commit

---
+++ kata/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  kata
+//
+//  Created by Colin Milhench on 01/10/2013.
+//  Copyright (c) 2013 Colin Milhench. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
